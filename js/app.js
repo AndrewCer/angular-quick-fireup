839fe12c8b726746373012fbb5a2c3dbf734c1d4
Andrew Cer <cer.andrew@gmail.com>
2015-08-22 10:17:59 -0600

    changed base setup

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 var app = angular.module("appName", ['ngRoute']);
+
+
+app.config(function ($routeProvider, $locationProvider) {
+
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/home.html',
+      controller: 'HomeController'
+    })
+    $locationProvider.html5Mode(true);
+    // .otherwise({redirectTo: '/'})
+})
